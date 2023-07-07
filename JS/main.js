23c13af073921f5baf40eefcbad3d8429df77c20
tadie-chiwy <127861828+tadie-chiwy@users.noreply.github.com>
2023-07-07 10:27:05 +0200

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,52 @@
+$(document).on('ready', function () {
+    $(".slicker").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3.5,
+        slidesToScroll: 1.5,
+        arrow:true,
+        nextArrow:".next-arrow-1",
+        prevArrow:".prev-arrow-1"
+
+      });
+});
+
+
+
+
+
+$(document).on('ready', function () {
+  $(".chiweshe").slick({
+      dots: true,
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 2,
+      arrow:true,
+      nextArrow:".next-arrow-2",
+      prevArrow:".prev-arrow-2"
+
+    });
+});
+
+
+
+$(document).on('ready', function () {
+  $(".tadiwa").slick({
+      dots: true,
+      infinite: true,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      arrow:true,
+      nextArrow:".next-arrow",
+      prevArrow:".prev-arrow"
+
+    });
+});
+
+
+
+
+
+
+
+
